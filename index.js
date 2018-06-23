1374fda8063f7fbdd571ce9966adc2f4c5c6e38d
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2018-06-23 18:09:19 +0900

    turn on tv

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const config = require('./config').config;
 const wol = require('wake_on_lan');
-const lgtv = require("lgtv2")({
-    url: `ws://${config.LGTVIP}:3000`,
+const express = require('express');
+let app = express();
+
+app.get('/LGTV/turnOn', function (req, res) {
+    wol.wake(config.LGTVMAC);
+    res.send('ok');
 });
 
-wol.wake(config.LGTVMAC);
-
-lgtv.on('connect', () => {
-    lgtv.request('ssap://system/turnOff', function (err, res) {
-        lgtv.disconnect();
-    });
+app.listen(3000, function () {
+    console.log('Server listening on port 3000!');
 });
 
